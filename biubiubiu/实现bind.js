b0cc37b8870073aa1842cf01ea78d04e0b8ba220
jiangtao <jiangtao@weidian.com>
2021-04-06 14:57:47 +0800

    feat: 🎸 add bind手动实现

---
+++ biubiubiu/实现bind.js
@@ -0,0 +1,32 @@
+Function.prototype.mbind = function (thisArgs) {
+    const fn = this
+    const args = Array.prototype.slice.call(arguments, 1)
+    const bound = function () {
+        const context = new.target ? this : thisArgs
+        return fn.apply(context, args.concat(Array.prototype.slice.call(arguments)))
+    }
+
+    bound.prototype = fn.prototype
+
+    return bound
+}
+
+const test = {
+    z: 1
+}
+
+function fn(x, y) {
+    this.name = '听风是风';
+    console.log(this.z);
+    console.log(x);
+    console.log(y);
+};
+
+
+const bound = fn.mbind(test)
+
+console.log(bound(1, 2)) // 1,1,2
+
+const person = new bound(1, 2) // undefined,1,2
+
+console.log(person) // { name: '听风是风'}
